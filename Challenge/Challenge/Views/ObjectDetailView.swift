bb441f924cc6c98104eb2ea10035d73c6ad5ab57
GuillaumeDochy <guillaume.dochy@student.ehb.be>
2024-09-30 11:36:51 +0200

    gitignore

--- Challenge/Challenge/Views/ObjectDetailView.swift
+++ Challenge/Challenge/Views/ObjectDetailView.swift
@@ -10,7 +10,9 @@ import SwiftUI
 struct ObjectDetailView: View {
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ProgressView()
+            .progressViewStyle(CircularProgressViewStyle())
+        Text("Loading")
     }
 }
 
